533383630f8f4740722dde0883b7b06b27d3c995
kavitha <vkavitha434@gmail.com>
2023-12-14 17:10:19 +0530

    Added AmazonApplication

---
+++ src/main/java/com/simplylearn/seleniumtest/FiltersUsingTest.java
@@ -0,0 +1,36 @@
+package com.simplylearn.seleniumtest;
+
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class FiltersUsingTest {
+
+	static WebDriver driver;
+	public static void main(String[] args) throws InterruptedException {
+		String siteurl="https://www.amazon.in";
+		String driverpath="drivers/windows/geckodriver.exe";
+		System.setProperty("webdriver.geckodriver.driver", driverpath);
+		driver=new FirefoxDriver();
+		driver.get(siteurl);
+		checkFillterTest();
+		driver.close();
+
+	}
+	public static void checkFillterTest() throws InterruptedException {
+		WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+		driver.manage().window().maximize();
+		searchBox.sendKeys("Nokia");
+		searchBox.submit();
+		System.out.println(searchBox.toString());
+		
+		
+	
+		
+	}
+
+}
